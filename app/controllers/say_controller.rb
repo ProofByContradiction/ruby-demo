031970689c31faf5d3e52006dd16d9c22afd1e59
Matthew McKee <matthew@demandwell.com>
2022-07-05 13:09:29 -0400

    playing with blocks

--- app/controllers/say_controller.rb
+++ app/controllers/say_controller.rb
@@ -4,11 +4,20 @@ class SayController < ApplicationController
   end
 
   def goodbye
+    wrap { print "I hate elves! " }
     @future_time = 1.hour.from_now.localtime
     @files = Dir.glob('*')
   end
 
+  def wrap &b
+    print "Santa says: "
+    3.times(&b)
+    print "\n"
+  end
+  
+
   def goodnight(name)
     result 'Good night, ' + name
     return result 
+  end
 end
